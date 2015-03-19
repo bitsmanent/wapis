be7970d002acfaa3b9b259af09ab5aa461901eb8
clamiax <smoppy@gmail.com>
2015-03-19 18:58:09 +0100

    Add a test file to try things up

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+require('wapis.php');
+
+define('WWONLINE_APIKEY', '');
+define('FORECAST_APIKEY', '');
+
+function str2coords($str) {
+	$str = rawurlencode($str);
+	$uri = "http://maps.googleapis.com/maps/api/geocode/json?address=${str}&sensor=true";
+	$d = json_decode(file_get_contents($uri), 1);
+	return (@$d['results'][0]['geometry']['location']);
+}
+
+function main() {
+	$str = 'Palermo, Italy';
+	$t = str2coords($str);
+	if(!$t)
+		die("${str}: no coordinates found.\n");
+	$lat = $t['lat'];
+	$lon = $t['lng'];
+	$qi = [
+		'lat' => $lat,
+		'lon' => $lon,
+		'cnt' => 7,
+		'raw' => 0
+	];
+	$services = wapis_getall();
+	foreach($services as $s => $h)
+		echo strtoupper($s).":\n".print_r(wapis_get($s, $qi), 1)."\n\n";
+}
+
+main();
+?>
